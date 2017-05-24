024f2ef21117bfdf33c5218572e3f44ec6a00405
mendozadaniel <danielmendoza@Local-Network.local>
2017-05-24 00:30:04 -0700

    First commit

---
+++ photo_album/forms.py
@@ -0,0 +1,28 @@
+from django.forms import ModelForm
+
+from cloudinary.forms import CloudinaryJsFileField, CloudinaryUnsignedJsFileField
+# Next two lines are only used for generating the upload preset sample name
+from cloudinary.compat import to_bytes
+import cloudinary, hashlib
+
+from .models import Photo
+
+class PhotoForm(ModelForm):
+    class Meta:
+        model = Photo
+        fields = '__all__'
+
+class PhotoDirectForm(PhotoForm):
+    image = CloudinaryJsFileField(
+                options = {
+                    'width': 500, 'height': 500,
+                    'tags' : ['image_tagged', 'random_images']
+                })
+
+class PhotoUnsignedDirectForm(PhotoForm):
+    upload_preset_name = "sample_" + hashlib.sha1(to_bytes(cloudinary.config().api_key + cloudinary.config().api_secret)).hexdigest()[0:10]
+    image = CloudinaryUnsignedJsFileField(upload_preset_name,
+                options = {
+                    'width': 500, 'height': 500,
+                    'tags': ['image_tagged', 'random_images']
+                })
